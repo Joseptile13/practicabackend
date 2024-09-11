635121c0e4d80724b4bd372f64b61182d5ec2af1
Azurtilo <joseacunapuente14@gmail.com>
2024-09-11 16:41:12 -0300

    creacion 2 vistas funcionales en app1

--- joseacunaproject/app1/views.py
+++ joseacunaproject/app1/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    http = """
+    <h1>APP 1</h1>
+    <h2>Vista 1</h2>
+    <h3>joseacuna</h3>
+    """
+    return HttpResponse(http)
+
+def vista2(request):
+    http = """
+    <h1>APP 1</h1>
+    <h2>Vista 2</h2>
+    <h3>joseacuna_2</h3>
+    """
+    return HttpResponse(http)
